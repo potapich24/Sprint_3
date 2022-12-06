4c1191d4fa23c05ede70917e884f6200dd047524
Mikhail Andreev <potapich@yandex.ru>
2022-12-06 19:16:01 +0300

    Test_Registration

---
+++ tests/Entrance.py
@@ -0,0 +1,10 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions
+from selenium.webdriver.support.wait import WebDriverWait
+
+driver = webdriver.Chrome()
+driver.get("https://stellarburgers.nomoreparties.site")
+
+
+
