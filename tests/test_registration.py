6ca8865fdc650546129a0812931076a175bad641
Mikhail Andreev <potapich@yandex.ru>
2022-12-13 12:53:01 +0300

    fix_test_registration_and_test_transfer_to_you_account

---
+++ tests/test_registration.py
@@ -0,0 +1,68 @@
+import random
+
+from locators import LocatorsForRegistration
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions
+from selenium.webdriver.support.wait import WebDriverWait
+
+def test_registration():
+    driver = webdriver.Chrome()
+    driver.get("https://stellarburgers.nomoreparties.site")
+
+    #Нажимаем на кнопку "Войти в аккаунт"
+    driver.find_element(*LocatorsForRegistration.BUTTON_SIGN_IN_TO_ACCOUNT).click()
+    #Нажимаем на "Зарегистрироваться"
+    driver.find_element(*LocatorsForRegistration.BUTTON_REGISTRATION).click()
+
+    #Генерируем логин
+    new_login = f"mihailandreev{random.randint(0, 999)}@yandex.ru"
+
+    #Заполняем поле "Имя"
+    driver.find_element(*LocatorsForRegistration.FIELD_NAME).send_keys(("Mihail"))
+
+    #Проверяем, что поле "Имя" не пустое
+    assert driver.find_element(*LocatorsForRegistration.FIELD_NAME).get_attribute('value') == 'Mihail'
+
+    #Вводим логин в поле "Логин"
+    driver.find_element(*LocatorsForRegistration.FIELD_LOGIN).send_keys(new_login)
+
+    #Проверяем, что в веденном значении поля "Логин" есть '@yandex.ru'
+    assert '@yandex.ru' in driver.find_element(*LocatorsForRegistration.FIELD_LOGIN).get_attribute('value')
+
+    #Заполняем поле "Пароль"
+    driver.find_element(*LocatorsForRegistration.FIELD_PASSWORD).send_keys("mihail55")
+
+    #Проверяем, что длина пароля больше или ровна 6
+    assert len(driver.find_element(*LocatorsForRegistration.FIELD_PASSWORD).get_attribute('value'))>=6
+
+    #Нажимаем кнопку "Зарегистрироваться"
+    driver.find_element(*LocatorsForRegistration.BUTTON_REGISTER).click()
+    WebDriverWait(driver, 3).until(expected_conditions.visibility_of_element_located((LocatorsForRegistration.HEADING_ENTRANCE)))
+
+    #Проверяем, что регистарация прошла успешно и на отркывшейся странице есть элемент с надписью "Вход"
+    assert driver.find_element(*LocatorsForRegistration.HEADING_ENTRANCE).text == 'Вход'
+    driver.quit()
+
+    driver = webdriver.Chrome()
+    driver.get("https://stellarburgers.nomoreparties.site")
+
+    #Входим в аккаунт
+    driver.find_element(*LocatorsForRegistration.BUTTON_SIGN_IN_TO_ACCOUNT).click()
+    #Нажимаем на кнопку "Зарегистрироваться"
+    driver.find_element(*LocatorsForRegistration.BUTTON_REGISTRATION).click()
+
+    #Заполняем поле "Пароль". Длина меньше 6
+    driver.find_element(*LocatorsForRegistration.FIELD_PASSWORD).send_keys("miha")
+
+    #Нажимаем кнопку "Зарегистрироваться"
+    driver.find_element(*LocatorsForRegistration.BUTTON_REGISTER).click()
+
+    #Проверяем, что всплывает ошибка "Некорректный пароль", если ввести пароль менее 6 символов
+    assert driver.find_element(*LocatorsForRegistration.MISTAKE_PASSWORD).text == 'Некорректный пароль'
+    assert driver.find_element(*LocatorsForRegistration.MISTAKE_PASSWORD).is_displayed()
+    driver.quit()
+
+test_registration()
+
---
+++ tests/test_transfer_to_your_personal_account.py
@@ -0,0 +1,35 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions
+from selenium.webdriver.support.wait import WebDriverWait
+
+from locators import LocatrsForTransferToYouPersonalAccount
+from locators import LocatorsForRegistration
+from locators import LocatorsForEntrance
+
+def test_transfer_to_you_personal_account():
+    driver = webdriver.Chrome()
+    driver.get("https://stellarburgers.nomoreparties.site")
+
+    #Нажимаем на кнопку "Войти в аккаунт"
+    driver.find_element(*LocatorsForRegistration.BUTTON_SIGN_IN_TO_ACCOUNT).click()
+
+    #Вводим Email
+    driver.find_element(*LocatorsForEntrance.FIELD_EMAIL).send_keys('mihailandreev5@yandex.ru')
+    #Вводим пароль
+    driver.find_element(*LocatorsForEntrance.FIELD_PASSWORD).send_keys('mihail55')
+    #Нажимаем войти
+    driver.find_element(*LocatorsForEntrance.BUTTON_ENTRANCE).click()
+
+    WebDriverWait(driver, 3).until(expected_conditions.visibility_of_element_located((LocatorsForEntrance.HEADING_ASSEMBLE_THE_BURGER)))
+
+    #Входим в личный кабинет
+    driver.find_element(*LocatorsForEntrance.BUTTON_PERSONAL_ACCOUNT).click()
+
+    #Проверям, что открылась страница "Личный кабинет"
+    WebDriverWait(driver, 3).until(expected_conditions.visibility_of_element_located((LocatrsForTransferToYouPersonalAccount.HEADING_PROFILE)))
+    assert driver.find_element(*LocatrsForTransferToYouPersonalAccount.HEADING_PROFILE).text == 'Профиль'
+    assert driver.find_element(*LocatrsForTransferToYouPersonalAccount.HEADING_PROFILE).is_displayed()
+    driver.quit()
+
+test_transfer_to_you_personal_account()
